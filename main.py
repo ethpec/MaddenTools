7ede7fffe3a40a5f7fa8a077614eb51ec29bddc2
Ian Peck <ianpeck22@gmail.com>
2022-01-22 09:44:15 -0600

    Add Windows VENV

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import xlrd
 
-file_path = '/Users/ianjpeck/Documents/GitHub/madden22stats/Files/All.xlsm'
+file_path = 'Files\All.xlsm'
 
 # YOU MUST PUT sheet_name=None TO READ ALL CSV FILES IN YOUR XLSM FILE
 df_players = pd.read_excel(file_path, sheet_name='124 Stuff')
