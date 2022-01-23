1c280f1db07f51fb69afe98f31b90c0fcd864dd4
Ian Peck <ianpeck22@gmail.com>
2022-01-22 23:31:52 -0600

    added df and test function

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import xlrd
 
 file_path = 'Files/All.xlsm'
 
+# Tiers
+tier_0 = range(95,100)
+tier_1 = range(90,94)
+
 # YOU MUST PUT sheet_name=None TO READ ALL CSV FILES IN YOUR XLSM FILE
 df_players = pd.read_excel(file_path, sheet_name='124 Stuff')
+# add your code for the other sheets below with df_names
 
 # prints all sheets
 print(df_players)
+
+def proregstats():
+    for row in df_players:
+        if int(df_players['Rating']) in tier_0:
+            df_players['Regression'] + 1 
