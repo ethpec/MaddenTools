c20423b4df705548885686cbef354d6752760129
Ian Peck <ianpeck22@gmail.com>
2022-01-27 21:31:46 -0600

    Add Loop and some logic for WR

--- main.py
+++ main.py
@@ -27,33 +27,46 @@ df_offensiveStats = pd.read_excel(file_path, sheet_name='Offensive Stats').merge
 df_defensiveStats = pd.read_excel(file_path, sheet_name='Defensive Stats').merge(df_players, how='right', left_on=['FullName', 'Position'], right_on=['FullName','Position'])
 df_olineStats = pd.read_excel(file_path, sheet_name='OLine Stats').merge(df_players, how='right', left_on=['FullName', 'Position'], right_on=['FullName','Position'])
 
+# Add new DataFrame columns
 
-# Test Function for looking at ratings in the tiers
-# def checkrating(rating):
-#     if rating in tier_0:
-#         print(str(rating) + ' is in tier 0')
-#     elif rating in tier_1:
-#         print(str(rating) + ' is in tier 1')
-#     elif rating in tier_2:
-#         print(str(rating) + ' is in tier 2')
-#     elif rating in tier_3:
-#         print(str(rating) + ' is in tier 3')
-#     elif rating in tier_4:
-#         print(str(rating) + ' is in tier 4')
-#     elif rating in tier_5:
-#         print(str(rating) + ' is in tier 5')
-#     elif rating in tier_6:
-#         print(str(rating) + ' is in tier 6')
-#     else:
-#         print('This is not a valid rating!')
 
-# Progression Function to be used on every row of Player Dataframe
-def progression(df_players):
-    if df_players.Position == 'HB':
-        pass        
+# Offensive Stats/Progression
 
-# # Apply the function to our Player Dataframe "row by row" (axis = 1)
-# df_players.apply(progression, axis=1)
+for idx, row in df_offensiveStats.iterrows():
+    # Running Backs
+    if  df_offensiveStats.loc[idx,'Position'] == 'WR':
+        # Tier 0
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_0:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 1
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_1:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 2
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_2:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 3
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_3:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 4
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_4:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 5
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_5:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Tier 6
+        if df_offensiveStats.loc[idx,'OverallRating'] in tier_6:
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+        # Test
+        if df_offensiveStats.loc[idx,'FullName'] == 'Davante Adams':
+            df_offensiveStats.loc[idx,'SkillPoints'] += 1
+
+    if  df_offensiveStats.loc[idx,'Position'] == 'HB':
+        df_offensiveStats.loc[idx,'SkillPoints'] = 1
+    if  df_offensiveStats.loc[idx,'Position'] == 'TE':
+        df_offensiveStats.loc[idx,'SkillPoints'] = 1
+
+
+# Join worksheet DataFrames to player DataFrame
 
 # Export our new sheet to a file
 df_offensiveStats.to_csv('Files/Test.csv', sep=',',index=False)
