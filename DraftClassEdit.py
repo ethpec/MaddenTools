01518ea87cc51ac05fd0e67f36da555d9e53ddc3
Ethan Peck <ethpec@gmail.com>
2023-08-30 23:02:09 -0500

    Draft Class Edit Added

--- DraftClassEdit.py
+++ DraftClassEdit.py
@@ -11,27 +11,64 @@ def update_traits(row):
     # Check the player's position and apply changes to specific columns
     contract_status = row['ContractStatus']
     
-    if contract_status in ['Draft']:
+    if contract_status in ['FreeAgent', 'Signed', 'PracticeSquad']:
+
+        # QB Edits
         if row['Position'] == 'QB':
-            row['Trait_ThrowAway'] = 'TRUE'  # Change Trait_ThrowAway column for QBs to 'TRUE'
-        elif row['Position'] == 'HB':
+            # For QBs, set a minimum of 70 and a maximum of 80 for InjuryRating
+            new_injury_rating = row['InjuryRating'] - 17
+            # Ensure the new value is within the specified range
+            if new_injury_rating < 70:
+                new_injury_rating = 70
+            if new_injury_rating > 80:
+                new_injury_rating = 80
+            row['InjuryRating'] = new_injury_rating
+            row['TRAIT_THROWAWAY'] = 'TRUE'  # Change Trait_ThrowAway column for QBs to 'TRUE'
+            row['TRAIT_COVER_BALL'] = 'ForAllHits'
+            if 'Pocket' in row['TRAIT_QBSTYLE']:
+                row['TRAIT_QBSTYLE'] = 'Balanced'
+            if row['SpeedRating'] < 80:
+                row['TRAIT_QBSTYLE'] = 'Balanced'
+            if row['SpeedRating'] >= 87:
+                row['TRAIT_QBSTYLE'] = 'Scrambler'
+
+        # HB Edits
+        if row['Position'] == 'HB':
             # For HBs, set a minimum of 73 and a maximum of 85 for InjuryRating
-            new_injury_rating = row['InjuryRating'] - 13
+            new_injury_rating = row['InjuryRating'] - 12
             # Ensure the new value is within the specified range
             if new_injury_rating < 73:
                 new_injury_rating = 73
-            elif new_injury_rating > 85:
+            if new_injury_rating > 85:
                 new_injury_rating = 85
             row['InjuryRating'] = new_injury_rating
-        else:
-            # For all other positions, set a minimum of 68 and a maximum of 80 for InjuryRating
-            new_injury_rating = row['InjuryRating'] - 18
+            row['TRAIT_YACCATCH'] = 'TRUE'
+            row['TRAIT_POSSESSIONCATCH'] = 'TRUE'
+            row['TRAIT_HIGHPOINTCATCH'] = 'TRUE'
+
+        # OFF Edits
+        if row['Position'] in ['WR', 'TE']:
+            row['TRAIT_YACCATCH'] = 'TRUE'
+            row['TRAIT_POSSESSIONCATCH'] = 'TRUE'
+            row['TRAIT_HIGHPOINTCATCH'] = 'TRUE'
+
+        # DEF Edits
+        if row['Position'] in ['LE', 'RE', 'DT']:
+            row['TRAIT_DLSWIM'] = 'TRUE'
+            row['TRAIT_DLSPIN'] = 'TRUE'
+            row['TRAIT_DLBULLRUSH'] = 'TRUE'
+
+        # For all other positions, set a minimum of 68 and a maximum of 80 for InjuryRating
+        if row['Position'] not in ['HB', 'QB']:
+
+            new_injury_rating = row['InjuryRating'] - 17
             # Ensure the new value is within the specified range
             if new_injury_rating < 68:
                 new_injury_rating = 68
-            elif new_injury_rating > 80:
+            if new_injury_rating > 80:
                 new_injury_rating = 80
             row['InjuryRating'] = new_injury_rating
+
     # Add more conditions and changes for other columns and positions as needed
     return row
 
