a3e41583f8b4ee7f2aa4d146f3cd4107a2cf3350
Ethan Peck <ethpec@gmail.com>
2023-08-30 22:36:45 -0500

    Preseason Trait Check Additions

--- PreseasonTraitCheck.py
+++ PreseasonTraitCheck.py
@@ -12,8 +12,21 @@ def update_traits(row):
     contract_status = row['ContractStatus']
     
     if contract_status in ['FreeAgent', 'Signed', 'PracticeSquad']:
+
+        # QB Edits
         if row['Position'] == 'QB':
-            row['Trait_ThrowAway'] = 'TRUE'  # Change Trait_ThrowAway column for QBs to 'TRUE'
+            row['TRAIT_THROWAWAY'] = 'TRUE'  # Change Trait_ThrowAway column for QBs to 'TRUE'
+            row['TRAIT_COVER_BALL'] = 'ForAllHits'
+            if 'Pocket' in row['TRAIT_QBSTYLE']:
+                row['TRAIT_QBSTYLE'] = 'Balanced'
+            if row['SpeedRating'] < 80:
+                row['TRAIT_QBSTYLE'] = 'Balanced'
+            elif row['SpeedRating'] >= 85:
+                row['TRAIT_QBSTYLE'] = 'Scrambler'
+            if row['InjuryRating'] < 70:
+                row['InjuryRating'] = 70
+
+        # HB Edits
         elif row['Position'] == 'HB':
             # For HBs, set a minimum of 73 and a maximum of 85 for InjuryRating
             new_injury_rating = row['InjuryRating']
@@ -23,6 +36,22 @@ def update_traits(row):
             elif new_injury_rating > 85:
                 new_injury_rating = 85
             row['InjuryRating'] = new_injury_rating
+            row['TRAIT_YACCATCH'] = 'TRUE'
+            row['TRAIT_POSSESSIONCATCH'] = 'TRUE'
+            row['TRAIT_HIGHPOINTCATCH'] = 'TRUE'
+
+        # OFF Edits
+        elif row['Position'] in ['WR', 'TE']:
+            row['TRAIT_YACCATCH'] = 'TRUE'
+            row['TRAIT_POSSESSIONCATCH'] = 'TRUE'
+            row['TRAIT_HIGHPOINTCATCH'] = 'TRUE'
+
+        # DEF Edits
+        elif row['Position'] in ['LE', 'RE', 'DT']:
+            row['TRAIT_DLSWIM'] = 'TRUE'
+            row['TRAIT_DLSPIN'] = 'TRUE'
+            row['TRAIT_DLBULLRUSH'] = 'TRUE'
+
         else:
             # For all other positions, set a minimum of 68 and a maximum of 80 for InjuryRating
             new_injury_rating = row['InjuryRating']
@@ -32,11 +61,12 @@ def update_traits(row):
             elif new_injury_rating > 80:
                 new_injury_rating = 80
             row['InjuryRating'] = new_injury_rating
+
     # Add more conditions and changes for other columns and positions as needed
     return row
 
 # Apply the new function to update the DataFrame
 df = df.apply(update_traits, axis=1)
 
-output_filename = 'PreseasonTraitCheck.xlsx'
-df.to_excel('Files/Madden24/IE/Test/PreseasonTraitCheck.xlsx', index=False)
+output_filename = 'Player_PreseasonEdits.xlsx'
+df.to_excel('Files/Madden24/IE/Test/Player_PreseasonEdits.xlsx', index=False)
