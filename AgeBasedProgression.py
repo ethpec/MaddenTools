b2b18099e42b1efb186db8737fa4081251ba9adf
Ethan P <ethpec@gmail.com>
2023-12-11 23:24:57 -0600

    Season0 Progression

--- AgeBasedProgression.py
+++ AgeBasedProgression.py
@@ -1,37 +1,41 @@
-# Imports
 import pandas as pd
 import random
 
 # Your File Path
-file_path = 'Files/Madden24/IE/Test/Final_AllStatBased.csv'
+file_path = 'Files/Madden24/IE/Season0/Final_AllStatBased.csv'
 
 df = pd.read_csv(file_path)
 
 def calculate_age_based_skill_points(row):
-    if row['YearsPro'] in [0, 1, 2, 3] and row['ContractStatus'] in ['Signed', 'PracticeSquad'] and row['Age'] in [20, 21, 22, 23, 24, 25] and row['Position'] not in ['QB', 'K', 'P']:
+    if (
+        row['YearsPro'] in [0, 1, 2, 3]
+        and row['ContractStatus'] in ['Signed', 'PracticeSquad']
+        and row['Age'] in [20, 21, 22, 23, 24, 25]
+        and row['Position'] not in ['QB', 'K', 'P']
+    ):
         development_trait = row['TraitDevelopment']
         if development_trait == 'Normal':
             chances = [0, 1, 2, 3, 4, 5, 6]
             probabilities = [0.00, 0.635, 0.26, 0.085, 0.01, 0.01, 0.00]
             skill_points = random.choices(chances, probabilities)[0]
-            return skill_points
+            return row['SkillPoints'] + skill_points  # Add skill points to the existing value
         elif development_trait == 'Star':
-             chances = [0, 1, 2, 3, 4, 5, 6]
-             probabilities = [0.00, 0.10, 0.20, 0.40, 0.20, 0.10, 0.00]
-             skill_points = random.choices(chances, probabilities)[0]
-             return skill_points
+            chances = [0, 1, 2, 3, 4, 5, 6]
+            probabilities = [0.00, 0.10, 0.20, 0.40, 0.20, 0.10, 0.00]
+            skill_points = random.choices(chances, probabilities)[0]
+            return row['SkillPoints'] + skill_points  # Add skill points to the existing value
         elif development_trait == 'Superstar':
-             chances = [0, 1, 2, 3, 4, 5, 6]
-             probabilities = [0.00, 0.00, 0.00, 0.15, 0.35, 0.35, 0.15]
-             skill_points = random.choices(chances, probabilities)[0]
-             return skill_points
+            chances = [0, 1, 2, 3, 4, 5, 6]
+            probabilities = [0.00, 0.00, 0.00, 0.15, 0.35, 0.35, 0.15]
+            skill_points = random.choices(chances, probabilities)[0]
+            return row['SkillPoints'] + skill_points  # Add skill points to the existing value
         elif development_trait == 'XFactor':
-             chances = [0, 1, 2, 3, 4, 5, 6]
-             probabilities = [0.00, 0.00, 0.00, 0.15, 0.35, 0.35, 0.15]
-             skill_points = random.choices(chances, probabilities)[0]
-             return skill_points
+            chances = [0, 1, 2, 3, 4, 5, 6]
+            probabilities = [0.00, 0.00, 0.00, 0.15, 0.35, 0.35, 0.15]
+            skill_points = random.choices(chances, probabilities)[0]
+            return row['SkillPoints'] + skill_points  # Add skill points to the existing value
     return row['SkillPoints']  # Keep the existing skill points if conditions are not met
-    
+
 def calculate_freeagent_regression_points(row):
     if row['Age'] >= 26 and row['Position'] != 'QB' and row['ContractStatus'] == 'FreeAgent':
         return row['RegressionPoints'] + 3  # Add 3 points to the existing value
@@ -42,4 +46,4 @@ df['RegressionPoints'] = df.apply(calculate_freeagent_regression_points, axis=1)
 df['SkillPoints'] = df.apply(calculate_age_based_skill_points, axis=1)
 
 output_filename = 'Final.csv'
-df.to_csv('Files/Madden24/IE/Test/Final.csv', index=False)
+df.to_csv('Files/Madden24/IE/Season0/Final.csv', index=False)
--- StatBasedEditor.py
+++ StatBasedEditor.py
@@ -6,10 +6,10 @@ import sqlite3
 
 
 # Your File Path
-file_path = 'Files/Madden24/IE/Test/All.xlsm'
+file_path = 'Files/Madden24/IE/Season0/All.xlsm'
 
 # Season
-season = 1
+season = 0
 
 # Rating Tier
 tier_0 = range(95,100)
@@ -73,13 +73,13 @@ def trim_all_columns(df):
 df_players = pd.read_excel(file_path, sheet_name='Player Info')
 df_players['TeamName'] = df_players['TeamIndex'].apply(lambda x: team_dict[x]) # Create column with lambda (returns the key in our team_dict for every row. Say it sees a 0 in a row, it will make a column for that row and enter CHI into it.)
 df_players['RatingTier'] = df_players['OverallRating'].apply(find_rating_tier) # applies our function to every row in the column and creates a new column based on its result
-df_players.to_csv('Files/Madden24/IE/Test/PlayerTest.csv', sep=',',index=False)
+df_players.to_csv('Files/Madden24/IE/Season0/PlayerTest.csv', sep=',',index=False)
 
 # Excel Sheets Dataframe (Logic)
-df_logic = pd.read_excel('Files/Madden24/IE/Test/ProgRegLogicCheck.xlsx', sheet_name='Sheet1')
+df_logic = pd.read_excel('Files/Madden24/IE/Season0/ProgRegLogicCheck.xlsx', sheet_name='Sheet1')
 df_logic['StatHigh'] = df_logic['StatValue'].apply(make_high)
 df_logic['StatLow'] = df_logic['StatValue'].apply(make_low)
-df_logic.to_csv('Files/Madden24/IE/Test/LogicTest.csv', sep=',',index=False)
+df_logic.to_csv('Files/Madden24/IE/Season0/LogicTest.csv', sep=',',index=False)
 
 # Excel Sheet Dataframes (Stats) and JOINS
 df_offensiveStats = pd.read_excel(file_path, sheet_name='Offensive Stats').merge(df_players, how='left', left_on=['FullName', 'Position', 'TeamPrefixName'], right_on=['FullName','Position','TeamName'])
@@ -116,7 +116,7 @@ df_kickingStats['NetYardsToPuntYards'] = df_kickingStats['PUNTNETYARDS'] / df_ki
 
 # Add new DataFrame columns for Defense
 df_defensiveStats['DLSacksAndTFLPer1000Snaps'] = (((1.75 * df_defensiveStats['DLINESACKS']) + df_defensiveStats['DEFTACKLESFORLOSS']) / df_defensiveStats['DOWNSPLAYED']) * 1000
-df_defensiveStats['DTSacksAndTFLPer1000Snaps'] = (((2.25 * df_defensiveStats['DLINESACKS']) + df_defensiveStats['DEFTACKLESFORLOSS']) / df_defensiveStats['DOWNSPLAYED']) * 1000
+df_defensiveStats['DTSacksAndTFLPer1000Snaps'] = (((2.2 * df_defensiveStats['DLINESACKS']) + df_defensiveStats['DEFTACKLESFORLOSS']) / df_defensiveStats['DOWNSPLAYED']) * 1000
 df_defensiveStats['TotalTurnoversPer1000Snaps'] = ((df_defensiveStats['DLINEFUMBLERECOVERIES'] + df_defensiveStats['DLINESAFETIES'] + df_defensiveStats['DSECINTS'] + df_defensiveStats['DSECINTTDS'] + df_defensiveStats['DLINEBLOCKS'] + df_defensiveStats['DLINEFORCEDFUMBLES'] + df_defensiveStats['DLINEFUMBLETDS'])/ df_defensiveStats['DOWNSPLAYED']) * 1000
 df_defensiveStats['LBSacksTFLPassDeflPer1000Snaps'] = ((df_defensiveStats['DLINESACKS'] + df_defensiveStats['DEFTACKLESFORLOSS'] + df_defensiveStats['DEFPASSDEFLECTIONS']) / df_defensiveStats['DOWNSPLAYED']) * 1000
 df_defensiveStats['TacklesPer1000Snaps'] = ((df_defensiveStats['ASSDEFTACKLES'] + df_defensiveStats['DEFTACKLES']) / df_defensiveStats['DOWNSPLAYED']) * 1000
@@ -136,7 +136,7 @@ INNER JOIN df_logic df2 ON (df1.StatCheck = df2.StatCheck) AND (df1.Position = d
 ''' # our query
 df_off_points = pd.read_sql_query(qry_off,conn) # read query into a new DataFrame
 df_off_points_agg = df_off_points.groupby(['FullName','Position','TeamName'])['SkillPointOff'].sum().reset_index() # add all the skill points up
-df_off_points_agg.to_csv('Files/Madden24/IE/Test/Points_off.csv', sep=',',index=False)
+df_off_points_agg.to_csv('Files/Madden24/IE/Season0/Points_off.csv', sep=',',index=False)
 df_offensiveStats = df_offensiveStats.merge(df_off_points_agg, how='left', left_on=['FullName', 'Position','TeamPrefixName'], right_on=['FullName','Position','TeamName'])
 
 # Melt Defensive DataFrame
@@ -151,7 +151,7 @@ INNER JOIN df_logic df2 ON (df1.StatCheck = df2.StatCheck) AND (df1.Position = d
 ''' # our query
 df_def_points = pd.read_sql_query(qry_def,conn) # read query into a new DataFrame
 df_def_points_agg = df_def_points.groupby(['FullName','Position','TeamName'])['SkillPointDef'].sum().reset_index() # add all the skill points up
-df_def_points_agg.to_csv('Files/Madden24/IE/Test/Points_def.csv', sep=',',index=False)
+df_def_points_agg.to_csv('Files/Madden24/IE/Season0/Points_def.csv', sep=',',index=False)
 df_defensiveStats = df_defensiveStats.merge(df_def_points_agg, how='left', left_on=['FullName', 'Position','TeamPrefixName'], right_on=['FullName','Position','TeamName'])
 
 # Melt O-Line DataFrame
@@ -166,7 +166,7 @@ INNER JOIN df_logic df2 ON (df1.StatCheck = df2.StatCheck) AND (df1.Position = d
 ''' # our query
 df_oline_points = pd.read_sql_query(qry_oline,conn) # read query into a new DataFrame
 df_oline_points_agg = df_oline_points.groupby(['FullName','Position','TeamName'])['SkillPointOL'].sum().reset_index() # add all the skill points up
-df_oline_points_agg.to_csv('Files/Madden24/IE/Test/Points_ol.csv', sep=',',index=False)
+df_oline_points_agg.to_csv('Files/Madden24/IE/Season0/Points_ol.csv', sep=',',index=False)
 df_olineStats = df_olineStats.merge(df_oline_points_agg, how='left', left_on=['FullName', 'Position','TeamPrefixName'], right_on=['FullName','Position','TeamName'])
 
 # Melt Kicking DataFrame
@@ -181,7 +181,7 @@ INNER JOIN df_logic df2 ON (df1.StatCheck = df2.StatCheck) AND (df1.Position = d
 ''' # our query
 df_kicking_points = pd.read_sql_query(qry_kicking,conn) # read query into a new DataFrame
 df_kicking_points_agg = df_kicking_points.groupby(['FullName','Position','TeamName'])['SkillPointKick'].sum().reset_index() # add all the skill points up
-df_kicking_points_agg.to_csv('Files/Madden24/IE/Test/Points_kick.csv', sep=',',index=False)
+df_kicking_points_agg.to_csv('Files/Madden24/IE/Season0/Points_kick.csv', sep=',',index=False)
 df_kickingStats = df_kickingStats.merge(df_kicking_points_agg, how='left', left_on=['FullName', 'Position','TeamPrefixName'], right_on=['FullName','Position','TeamName'])
 
 # Join worksheet DataFrames to player DataFrame
@@ -197,11 +197,11 @@ df_final.loc[df_final['SkillPoints'] < 0, 'RegressionPoints'] = abs(df_final['Sk
 df_final.loc[df_final['SkillPoints'] < 0, 'SkillPoints'] = 0
 
 # # Export our DataFrames to various test files
-df_offensiveStats.to_csv('Files/Madden24/IE/Test/OffTest.csv', sep=',',index=False)
-df_defensiveStats.to_csv('Files/Madden24/IE/Test/DefTest.csv', sep=',',index=False)
-df_olineStats.to_csv('Files/Madden24/IE/Test/OLTest.csv', sep=',',index=False)
-df_kickingStats.to_csv('Files/Madden24/IE/Test/KickingTest.csv', sep=',',index=False)
-# df_defensiveStats_unpivot.to_csv('Files/Madden24/IE/Test/Defense_Unpivot.csv', sep=',',index=False)
+df_offensiveStats.to_csv('Files/Madden24/IE/Season0/OffTest.csv', sep=',',index=False)
+df_defensiveStats.to_csv('Files/Madden24/IE/Season0/DefTest.csv', sep=',',index=False)
+df_olineStats.to_csv('Files/Madden24/IE/Season0/OLTest.csv', sep=',',index=False)
+df_kickingStats.to_csv('Files/Madden24/IE/Season0/KickingTest.csv', sep=',',index=False)
+# df_defensiveStats_unpivot.to_csv('Files/Madden24/IE/Season0/Defense_Unpivot.csv', sep=',',index=False)
 
 # Export our Final Player DataFrame with updated skills points/regression points
-df_final.to_csv('Files/Madden24/IE/Test/Final_PreAdjustment.csv', sep=',',index=False)
+df_final.to_csv('Files/Madden24/IE/Season0/Final_PreAdjustment.csv', sep=',',index=False)
