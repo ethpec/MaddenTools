e3ce1953fc2f680f814c1cf3f4fe2375ff333a10
Ethan P <ethpec@gmail.com>
2023-12-11 23:24:42 -0600

    Preseason Rookie Skill Point Adjustment

--- PreseasonRookieProgression.py
+++ PreseasonRookieProgression.py
@@ -11,18 +11,18 @@ def calculate_skill_points(row):
     if row['YearsPro'] == 0 and row['ContractStatus'] in ['FreeAgent', 'Signed', 'PracticeSquad'] and 'QB' not in row['Position']:
         development_trait = row['TraitDevelopment']
         if development_trait == 'Normal':
-            chances = [0, 1, 2, 3, 4, 5, 6]
-            probabilities = [0.20, 0.44, 0.20, 0.10, 0.04, 0.015, 0.005]
+            chances = [0, 1, 2, 3, 4, 5, 6, 8, 10]
+            probabilities = [0.20, 0.435, 0.20, 0.10, 0.04, 0.012, 0.007, 0.004, 0.002] ### Average = 1.5 ###
             skill_points = random.choices(chances, probabilities)[0]
             return skill_points
         elif development_trait == 'Star':
-             chances = [0, 1, 2, 3, 4, 5, 6]
-             probabilities = [0.00, 0.15, 0.475, 0.15, 0.125, 0.075, 0.025]
+             chances = [0, 1, 2, 3, 4, 5, 6, 8, 10]
+             probabilities = [0.00, 0.125, 0.45, 0.125, 0.125, 0.075, 0.05, 0.035, 0.015] ### Average = 3 ###
              skill_points = random.choices(chances, probabilities)[0]
              return skill_points
         elif development_trait == 'Superstar':
-             chances = [0, 1, 2, 3, 4, 5, 6]
-             probabilities = [0.00, 0.00, 0.00, 0.45, 0.30, 0.15, 0.10]
+             chances = [0, 1, 2, 3, 4, 5, 6, 8, 10]
+             probabilities = [0.00, 0.00, 0.00, 0.40, 0.25, 0.125, 0.10, 0.075, 0.05] ### Average = 4.5 ###
              skill_points = random.choices(chances, probabilities)[0]
              return skill_points
     return row['SkillPoints']  # Keep the existing skill points if conditions are not met
