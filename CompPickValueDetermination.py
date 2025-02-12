239ed2cea00efe1c1ed82929706691b2b23d1a68
Ethan <ethpec@gmail.com>
2025-02-11 22:49:05 -0600

    Season 7 Comp Picks

--- CompPickValueDetermination.py
+++ CompPickValueDetermination.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 # Your File Paths
-file_path = 'Files/Madden24/IE/Season7/Player.xlsx'
-all_pros_path = 'Files/Madden24/IE/Season7/AllPros.xlsx'
-all_xlsm_path = 'Files/Madden24/IE/Season7/All.xlsm'
+file_path = 'Files/Madden25/IE/Season7/Player.xlsx'
+all_pros_path = 'Files/Madden25/IE/Season7/AllPros.xlsx'
+all_xlsm_path = 'Files/Madden25/IE/Season7/All.xlsm'
 
 # Specify the current season year
-current_season_year = 4  ####### Change this to the correct value #######
+current_season_year = 7  ####### Change this to the correct value ####### (MAKE SURE THE DOWNSPLAYED PART IS BEING CALCULATED)
 
 def calculate_number_value_ranking(file_path, all_pros_path, all_xlsm_path):
     """
@@ -192,4 +192,4 @@ result_df['CompRank'] = result_df['TotalPoints'].rank(method='min', ascending=Fa
 
 # Export the resulting DataFrame to an Excel document
 output_filename = 'CompPickPlayerValue.xlsx'
-result_df.to_excel('Files/Madden24/IE/Season7/CompPickPlayerValue.xlsx', index=False)
+result_df.to_excel('Files/Madden25/IE/Season7/CompPickPlayerValue.xlsx', index=False)
