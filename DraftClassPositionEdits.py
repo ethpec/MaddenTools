16737aba980bdf9cd7e2b1ee4f3c3abe069c9685
Ethan <ethpec@gmail.com>
2025-04-06 16:49:53 -0500

    Added each position with desired attributes

--- DraftClassPositionEdits.py
+++ DraftClassPositionEdits.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Your File Path
-file_path = 'Files/Madden25/IE/Season8/Player.xlsx'
+file_path = 'Files/Madden25/IE/Season9/Player.xlsx'
 
 df = pd.read_excel(file_path)
 
@@ -12,14 +12,179 @@ def update_position_ratings(row):
     
     if contract_status in ['Draft']:
         # Change Edited Position Here
-        if row['Position'] in ['CB'] and row['OverallRating'] >= 64:
+        '''if row['Position'] in ['QB'] and row['OverallRating'] <= 0:
+            row['AwarenessRating'] += 1
+            row['ThrowAccuracyDeepRating'] += 1
+            row['ThrowAccuracyMidRating'] += 1
+            row['ThrowAccuracyShortRating'] += 1
+            row['ThrowPowerRating'] += 1
+            row['ThrowOnTheRunRating'] += 1
+            row['ThrowUnderPressureRating'] += 1'''
+
+        '''if row['Position'] in ['HB']:
+            row['AwarenessRating'] += 1
+            row['CarryingRating'] += 1
+            row['BreakTackleRating'] += 1
+            row['ThrowAccuracyShortRating'] += 1
+            row['StiffArmRating'] += 1
+            row['TruckingRating'] += 1
+            row['CatchingRating'] += 1'''
+
+        if row['Position'] in ['WR'] and row['OverallRating'] >= 60:
             row['AwarenessRating'] -= 1
-            row['PlayRecognitionRating'] -= 1
-            row['TackleRating'] -= 1
+            row['CatchingRating'] -= 1
+            row['DeepRouteRunningRating'] -= 1
+            row['MediumRouteRunningRating'] -= 1
+            row['ShortRouteRunningRating'] -= 1
+            row['ReleaseRating'] -= 1
+            row['CatchInTrafficRating'] -= 1
+            row['SpectacularCatchRating'] -= 1
+
+        '''if row['Position'] in ['TE'] and row['OverallRating'] >= 64:
+            row['AwarenessRating'] -= 1
+            row['CatchingRating'] -= 1
+            row['DeepRouteRunningRating'] -= 1
+            row['MediumRouteRunningRating'] -= 1
+            row['ShortRouteRunningRating'] -= 1
+            row['ReleaseRating'] -= 1
+            row['CatchInTrafficRating'] -= 1
+            row['SpectacularCatchRating'] -= 1'''
+
+        '''if row['Position'] in ['LT'] and row['OverallRating'] <= 58:
+            row['AwarenessRating'] += 1
+            row['ImpactBlockingRating'] += 1
+            row['LeadBlockRating'] += 1
+            row['RunBlockFinesseRating'] += 1
+            row['RunBlockPowerRating'] += 1
+            row['RunBlockRating'] += 1
+            row['PassBlockRating'] += 1
+            row['PassBlockFinesseRating'] += 1
+            row['PassBlockPowerRating'] += 1'''
+
+        '''if row['Position'] in ['LG'] and row['OverallRating'] >= 61:
+            row['AwarenessRating'] -= 1
+            row['ImpactBlockingRating'] -= 1
+            row['LeadBlockRating'] -= 1
+            row['RunBlockFinesseRating'] -= 1
+            row['RunBlockPowerRating'] -= 1
+            row['RunBlockRating'] -= 1
+            row['PassBlockRating'] -= 1
+            row['PassBlockFinesseRating'] -= 1
+            row['PassBlockPowerRating'] -= 1'''
+        
+        '''if row['Position'] in ['C']:
+            row['AwarenessRating'] += 1
+            row['ImpactBlockingRating'] += 1
+            row['LeadBlockRating'] += 1
+            row['RunBlockFinesseRating'] += 1
+            row['RunBlockPowerRating'] += 1
+            row['RunBlockRating'] += 1
+            row['PassBlockRating'] += 1
+            row['PassBlockFinesseRating'] += 1
+            row['PassBlockPowerRating'] += 1'''
+
+        '''if row['Position'] in ['RG'] and row['OverallRating'] <= 61:
+            row['AwarenessRating'] += 1
+            row['ImpactBlockingRating'] += 1
+            row['LeadBlockRating'] += 1
+            row['RunBlockFinesseRating'] += 1
+            row['RunBlockPowerRating'] += 1
+            row['RunBlockRating'] += 1
+            row['PassBlockRating'] += 1
+            row['PassBlockFinesseRating'] += 1
+            row['PassBlockPowerRating'] += 1'''
+
+        '''if row['Position'] in ['RT'] and row['OverallRating'] <= 59:
+            row['AwarenessRating'] += 1
+            row['ImpactBlockingRating'] += 1
+            row['LeadBlockRating'] += 1
+            row['RunBlockFinesseRating'] += 1
+            row['RunBlockPowerRating'] += 1
+            row['RunBlockRating'] += 1
+            row['PassBlockRating'] += 1
+            row['PassBlockFinesseRating'] += 1
+            row['PassBlockPowerRating'] += 1'''
+
+        '''if row['Position'] in ['LE'] and row['OverallRating'] >= 66:
+            row['AwarenessRating'] -= 1
+            row['BlockSheddingRating'] -= 1
+            row['FinesseMovesRating'] -= 1
             row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['PowerMovesRating'] -= 1'''
+
+        '''if row['Position'] in ['RE'] and row['OverallRating'] >= 62:
+            row['AwarenessRating'] -= 1
+            row['BlockSheddingRating'] -= 1
+            row['FinesseMovesRating'] -= 1
+            row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['PowerMovesRating'] -= 1'''
+
+        '''if row['Position'] in ['DT'] and row['OverallRating'] <= 65:
+            row['AwarenessRating'] += 1
+            row['BlockSheddingRating'] += 1
+            row['FinesseMovesRating'] += 1
+            row['PursuitRating'] += 1
+            row['TackleRating'] += 1
+            row['PlayRecognitionRating'] += 1
+            row['PowerMovesRating'] += 1'''
+
+        '''if row['Position'] in ['LOLB'] and row['OverallRating'] <= 60:
+            row['AwarenessRating'] += 1
+            row['PursuitRating'] += 1
+            row['TackleRating'] += 1
+            row['PlayRecognitionRating'] += 1
+            row['ManCoverageRating'] += 1
+            row['ZoneCoverageRating'] += 1
+            row['BlockSheddingRating'] += 1'''
+
+        '''if row['Position'] in ['ROLB'] and row['OverallRating'] <= 60:
+            row['AwarenessRating'] += 1
+            row['PursuitRating'] += 1
+            row['TackleRating'] += 1
+            row['PlayRecognitionRating'] += 1
+            row['ManCoverageRating'] += 1
+            row['ZoneCoverageRating'] += 1
+            row['BlockSheddingRating'] += 1'''
+
+        '''if row['Position'] in ['MLB'] and row['OverallRating'] >= 67:
+            row['AwarenessRating'] -= 2
+            row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['ManCoverageRating'] -= 1
+            row['ZoneCoverageRating'] -= 1
+            row['BlockSheddingRating'] -= 1'''
+
+        '''if row['Position'] in ['CB'] and row['OverallRating'] >= 64:
+            row['AwarenessRating'] -= 1
+            row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['ManCoverageRating'] -= 1
             row['PressRating'] -= 1
-            row['ZoneCoverageRating'] -= 2
-            row['ManCoverageRating'] -= 2
+            row['ZoneCoverageRating'] -= 1'''
+
+        '''if row['Position'] in ['FS'] and row['OverallRating'] >= 99:
+            row['AwarenessRating'] -= 1
+            row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['ManCoverageRating'] -= 1
+            row['PressRating'] -= 1
+            row['ZoneCoverageRating'] -= 1'''
+
+        '''if row['Position'] in ['SS']:
+            row['AwarenessRating'] -= 2
+            row['PursuitRating'] -= 1
+            row['TackleRating'] -= 1
+            row['PlayRecognitionRating'] -= 1
+            row['ManCoverageRating'] -= 1
+            row['PressRating'] -= 1
+            row['ZoneCoverageRating'] -= 1'''
     # Add more conditions and changes for other columns and positions as needed
     return row
 
@@ -40,4 +205,4 @@ for column in df.columns:
 df.drop(columns=columns_to_remove, inplace=True)
 
 output_filename = 'DraftClassPositionEdits.xlsx'
-df.to_excel('Files/Madden25/IE/Season8/DraftClassPositionEdits.xlsx', index=False)
+df.to_excel('Files/Madden25/IE/Season9/DraftClassPositionEdits.xlsx', index=False)
